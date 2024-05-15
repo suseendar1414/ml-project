c95a0660682bba42d3e288b8493a906f5c0ac874
suseendar1414 <suseendarannair@gmail.com>
2024-05-15 21:43:27 +0530

    second commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E__DOT='-e .'
+
+def get_requirements(file_path: str) -> List[str]: 
+    '''
+    this function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements= [req.replace("\n","") for req in requirements]
+
+        if HYPEN_E__DOT in requirements:
+            requirements.remove(HYPEN_E__DOT)
+
+    return requirements
+
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='Suseendar',
+author_email='suseendarannair@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
